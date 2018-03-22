0f4ee52e96458eac220fe854d6c9184f8bb8160d
Ricards Veveris (veverisr) <veverisr@coventry.ac.uk>
2018-03-22 11:02:17 +0000

    Update Webpage.py
    Actually calculates the calories burned correctly.

--- Webpage.py
+++ Webpage.py
@@ -14,44 +14,43 @@ class Website(object):
         with sql.connect(DB) as cur:
             results = cur.execute( '''SELECT date FROM Location;''')
             for date, in results:
-                if date[0:10] not in dates:
-                    dates.append(date[0:10])
-                print(date)
+                if date not in dates:
+                    dates.append(date)
         return dates
+    
+    def get_times(self):
+        with sql.connect(DB) as cur:
+            results = cur.execute( '''SELECT MIN(time), MAX(time) FROM Location GROUP BY date;''')
+            durations = []
+            for row in results:
+                print(row)
+                time_min, time_max = row
+                time_min = datetime.strptime(time_min[:8], "%H:%M:%S").time()
+                time_max = datetime.strptime(time_max[:8], "%H:%M:%S").time()
+                duration_time = datetime.combine(date.today(), time_max) - datetime.combine(date.today(), time_min)
+                (h, m, s) = str(duration_time).split(":")
+                duration_int = int(h) * 3600 + int(m) * 60 + int(s)
+                durations.append(duration_int)
+            return durations
     
     @cherrypy.expose
     def index(self,weight=60):                   
         template = JINJA_ENVIRONMENT.get_template('Charts.html')
         
-        
         dates = self.get_dates()
-        
-        
+        durations = self.get_times()
         data=[]
-        for row in dates:
-            time_min, time_max = self.get_times()
-            time_min = datetime.strptime(time_min[:7], "%H:%M:%S").time()
-            time_max = datetime.strptime(time_max[:7], "%H:%M:%S").time()
-            duration_time = datetime.combine(date.today(), time_max) - datetime.combine(date.today(), time_min)
-            (h, m, s) = str(duration_time).split(":")
-            duration_int = int(h) * 3600 + int(m) * 60 + int(s)
-            Cal = (weight * 9) * duration_int
-            
-            data.append( (row,Cal) )
-                #break
-        #data =  [("1/12/18",100),
-                #("2/12/18",200),
-                #("3/12/18",400)]
-       # print(data)
+        print(len(dates))
+        for e in range(len(durations)):
+            Cal = (weight * 9) * durations[e]
+            print(Cal)
+            data.append( (dates[e],Cal) )
         template_values = {
             'data' : data}
         return template.render(template_values)
     #Cal burnt = (weight in kg * activity value) * time doing
-    def get_times(self):
-        with sql.connect(DB) as cur:
-            results = cur.execute( '''SELECT MIN(time), MAX(time) FROM Location GROUP BY date;''')
-            for row in results:
-                return row
+    
+    
        
     
 if __name__ == '__main__':
