95af3c81e13b02e142fb777d809fd129da80ea6d
Ricards Veveris (veverisr) <veverisr@coventry.ac.uk>
2018-03-29 06:33:25 +0100

    Update Subscriber.py

--- Subscriber.py
+++ Subscriber.py
@@ -3,30 +3,27 @@ import sqlite3 as sql
 try:
     import paho.mqtt.client as mqtt
 except:
-    os.system('sudo pip3 install paho-mqtt')
+    os.system('sudo pip3 install paho-mqtt')    #if module not present, installs it and runs again
     os.system('python3 Subscriber.py')
 from datetime import date, datetime
+
+"""connects to the data topic"""
 def on_connect(client, userdata, flag, rc):
         client.subscribe("owntracks/user/ALL Project2")
 
-def on_connect(client, userdata, flag, rc):
-        client.subscribe("owntracks/user/nanofork")
-        
+"""what is done every time a new message gets posted to the topic"""
 def on_message(client, userdata, msg):
-    con = sql.connect('location.db')
-    cur = con.cursor()
+    con = sql.connect('location.db') #opens the database, if not present, creates one
+    cur = con.cursor()  #for executing sql queries
     try:
-        cur.execute("CREATE TABLE Location(longitude NUMBER(10,6), latitude NUMBER(10,6), date VARCHAR2(20), time VARCHAR2(20));")
+        cur.execute("CREATE TABLE Location(longitude NUMBER(10,6), latitude NUMBER(10,6), date VARCHAR2(20), time VARCHAR2(20));") #creates the table with the necessary parameters for storing values
     except:
-        pass
+        pass #if it's already created, then use that one
     data = json.loads(msg.payload.decode("utf8"))
-    print(data["lon"],data["lat"])
     day = date.today()
-    dat = datetime.now()
-    times = datetime.time(dat)
-    print(day)
-    print(times)
-    cur.execute("INSERT INTO Location values(?,?,?,?);", (data["lon"], data["lat"], str(day), str(times)))
+    clock = datetime.now()
+    time = datetime.time(clock)
+    cur.execute("INSERT INTO Location values(?,?,?,?);", (data["lon"], data["lat"], str(day), str(time))) #puts the latitude, longitude from the posted message as well as the date and time when it was posted into the database
     con.commit()
     cur.close()
     con.close()
@@ -34,7 +31,7 @@ try:
     client = mqtt.Client()
     client.on_connect = on_connect
     client.on_message = on_message
-    client.connect("iot.eclipse.org")
-    client.loop_forever()
+    client.connect("iot.eclipse.org") #connects throught the given broker
+    client.loop_forever()  #loops, so that new data can continuously be accepted
 except Exception as e:
     print(e)
